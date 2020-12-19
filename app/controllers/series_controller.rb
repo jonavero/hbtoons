877b90903c3644a1283e73d4cd2975acb8151d8a
marcos <marcosef24@gmail.com>
2020-12-18 16:49:09 -0800

    New Changes 18-12-2020

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -1,9 +1,9 @@
 class SeriesController < ApplicationController
-  before_action :requiere_usuario_admin, only: [:inicio,:new,:edit,:delete]
+  before_action :requiere_usuario_admin, only: [:inicio, :new, :edit, :delete]
 
 
   def inicio
-   @Series = Serie.paginate(:page => params[:page], :per_page => 16).order(:Categoria_id)
+    @Series = Serie.paginate(:page => params[:page], :per_page => 16).order(:Categoria_id)
 
   end
 
@@ -14,52 +14,63 @@ class SeriesController < ApplicationController
   def index
 
 
+    @Series = if params[:Titulo]
+                Serie.where('"Titulo" ILIKE ?', "%#{params[:Titulo]}%").where(Estatus: 'Activa')
 
-    @Series= if params[:Titulo]
-                  Serie.where('"Titulo" ILIKE ?',"%#{params[:Titulo]}%")
+              elsif params[:Categoria_id]
 
-               elsif  params[:Categoria_id]
+                Serie.where(Estatus: 'Activa').where('"Categoria_id" = ?', "#{params[:Categoria_id]}").paginate(:page => params[:page], :per_page => 16).order(:id)
+              else
 
-                    Serie.where('"Categoria_id" = ?',"#{params[:Categoria_id]}").paginate(:page => params[:page], :per_page => 16).order(:id)
-                else
-
-                  Serie.paginate(:page => params[:page], :per_page => 16).order(:id)
-                end
+                Serie.where(Estatus: 'Activa').paginate(:page => params[:page], :per_page => 16).order(:id)
+              end
 
 
-    @SeriesProxi= Serie.paginate(:page => params[:page], :per_page => 3).where(Estatus:'Proximamente')
+    @SeriesProxi = Serie.paginate(:page => params[:page], :per_page => 3).where(Estatus: 'Proximamente')
   end
 
 
   def show
-    @Serie= Serie.find(params[:id])
+
+    begin
+      @Serie = Serie.where(Estatus: 'Activa').find(params[:id])
+
+      @temporadas = @Serie.capitulos
+                        .where('"Serie_id" = ?', params[:id])
+                        .order(:temporada_id, :id)
+                        .group_by(&:temporada_id) # "1"
+
+
+      @Recomendadas = Serie.all.shuffle[0..2]
+
+      @Temporada = Temporada.find_by_sql(
+          "select  't.Descripcion' from temporadas as t inner join capitulos as c
+        on t.id=c.temporada_id
+        where 'c.serie_id' ='5'"
+
+      )
+
+    rescue
+      redirect_to "/"
+    end
+
+
+    # if @Serie.nil?
+    #   redirect_to "google.com" # action: "index"
+    # end
 
     # @Episodios= @Serie.capitulos.paginate(:page => params[:page], :per_page => 20).order(:Temporada, :Episodio)    #AQUUI
-    @temporadas = @Serie.capitulos
-                      .where('"Serie_id" = ?',params[:id])
-                      .order(:temporada_id, :id)
-                      .group_by(&:temporada_id) # "1"
 
-
-    @Recomendadas = Serie.all.shuffle[0..2]
-
-    @Temporada= Temporada.find_by_sql(
-        "select  't.Descripcion' from temporadas as t inner join capitulos as c
-    on t.id=c.temporada_id
-    where 'c.serie_id' ='5'"
-
-
-    )
 
   end
 
   def new
-    @Serie= Serie.new
-    @Estatus=['Proximamente','Activo','Inactivo']
+    @Serie = Serie.new
+    @Estatus = ['Proximamente', 'Activo', 'Inactivo']
   end
 
   def create
-    @Serie=Serie.new(params_serie)
+    @Serie = Serie.new(params_serie)
     if @Serie.save
       redirect_to '/inicio'
     else
@@ -69,28 +80,29 @@ class SeriesController < ApplicationController
   end
 
   def edit
-    @Serie=Serie.find(params[:id])
+    @Serie = Serie.find(params[:id])
   end
 
   def update
-    @Serie=Serie.find(params[:id])
-  if @Serie.update(params_serie)
-    redirect_to inicio_path, :notice=>'Registro actualizado'
-  else
-    render 'edit'
-  end
+    @Serie = Serie.find(params[:id])
+    if @Serie.update(params_serie)
+      redirect_to inicio_path, :notice => 'Registro actualizado'
+    else
+      render 'edit'
+    end
   end
 
   def delete
-    @Serie=Serie.find(params[:id])
+    @Serie = Serie.find(params[:id])
     if @Serie.destroy
       redirect_to inicio_path, :notice => 'Registro eliminado'
     end
   end
 
   private
+
   def params_serie
-    params.require(:serie).permit(:Titulo, :descripcion,:Idioma,:Ano,:Episodios,:Estatus,:Valoracion,:Categoria_id,:Portada,:Fondo)
+    params.require(:serie).permit(:Titulo, :descripcion, :Idioma, :Ano, :Episodios, :Estatus, :Valoracion, :Categoria_id, :Portada, :Fondo)
   end
 
 end
